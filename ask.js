41eab49f4fb0f06a0b36bb99d9f45fa98015ac6f
qianlong.zhao <qianlong.zhao@china.zhaogang.com>
2022-01-11 15:06:27 +0800

    1

--- ask.js
+++ ask.js
@@ -1,7 +1,5 @@
 module.exports = [
-  {
-    type: 'confirm',
-    name: 'private',
-    message: 'ths resgistery is private?',
-  }
+  { type: 'confirm', name: 'private', message: 'ths resgistery is private?' },
+  { type: 'input', name: 'author', message: 'author?' },
+  { type: 'input', name: 'license', message: 'license?' },
 ]
